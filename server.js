a59573df0e70aa97131070c4467f244c9d1570eb
Saurav Dakre <saurav.dakre@gmail.com>
2018-07-15 15:46:47 +0530

    integrated osprey

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
+const osprey = require('osprey');
 const express = require('express');
+const join = require('path').join;
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+const path = join(__dirname, 'assets', 'api.raml');
 
-app.listen(3000, () => {
-    console.log('server started at port 3000...');
-});
+osprey.loadFile(path)
+    .then((middleware) => {
+        app.use(middleware);
+        
+        app.get('/songs', (req, res) => {
+            res.send('Songs resource');
+        });
+        
+        app.listen(3000, () => {
+            console.log('server started at port 3000...');
+        });
+    })
+    .catch((error) => {
+        console.error("Error :", error);
+    })
